f095d49bebe85065b7f271555d01287bf659791c
Brady Holt <brady.holt@gmail.com>
2022-11-02 11:12:44 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# labeler-action
-GitHub Action that labels a PR based on the source branch name
+# Labeler Action
+
+GitHub Action that labels a PR based on the source branch name prefix
+
+## Inputs
+
+### `branch_label_mappings`
+
+**Required** A JSON formatted object that maps branch name prefixes to label names.
+
+**Default** `'{"enhancement/":"enhancement","bug/":"bug"}'`
+
+## Example usage
+
+```yaml
+uses: bradymholt/labeler-action@v1.0
+with:
+  branch_label_mappings: '{"enhancement/":"enhancement","bug/":"bug"}'
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const args = process.argv.slice(2);
+
+(async () => {
+  if (!github.context.payload.pull_request) {
+    core.setFailed(
+      `A pull_request object could not be found.  You must use a workflow trigger that includes a pull_request context.`
+    );
+    process.exit(1);
+  }
+
+  let branchLabelMappings = {};
+  try {
+    branchLabelMappings = JSON.parse(args[0]);
+  } catch {
+    core.setFailed(`The branch_label_mappings input is not formatted correctly.  It should be a JSON formatted object and able to be parsed when calling JSON.parse().`);
+    process.exit(1);
+  }
+
+  const branchName = github.context.payload.pull_request.head.ref;
+
+  console.log(`branch_label_mappings: ${JSON.stringify(branchLabelMappings)}`);
+  const mappedLabel = branchLabelMappings[Object.keys(branchLabelMappings).find(k=> branchName.startsWith(k))];
+
+  if (mappedLabel) {
+    console.log(`Will add label "${mappedLabel}" for branch name "${branchName}".`);
+
+    try {
+      const octokit = github.getOctokit(github.context.token);
+      await octokit.rest.issues.addLabels({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.payload.pull_request.number,
+        labels: [mappedLabel],
+      });
+    } catch (error) {
+      core.setFailed(`Failure occurred when adding label: ${error.message}`);
+    }
+  } else {
+    console.log(`No label mapping found for branch name "${branchName}".`);
+  }
+})();
